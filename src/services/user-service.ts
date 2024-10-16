15fd7c1a39b04ac88266df468914e4965073f155
Anderson <loonander@gmail.com>
2024-10-16 10:37:25 -0300

    feat: add proper error handling to create user service

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,18 +1,31 @@
 import * as argon2 from 'argon2';
-import { PrismaClient, User } from '@prisma/client';
+import { Prisma, User } from '@prisma/client';
 import { UserInput } from '../zod-schema/user-validation.js';
 import { UserValidationSchema } from '../zod-schema/user-validation.js';
 import { GraphQLError } from 'graphql';
-
-const prisma = new PrismaClient();
+import { prisma } from '../../prisma/prisma-client.js';
 
 export class UserService {
   async createUserService(params: UserInput): Promise<User> {
-    try {
-      UserValidationSchema.parse(params);
-      params.password = await argon2.hash(params.password);
+    const verification = UserValidationSchema.safeParse(params);
 
-      const { name, email, password, birthDate } = params;
+    if (!verification.success) {
+      const zoderrors = verification.error.errors.map((error) => ({
+        path: error.path.join('.'),
+        message: error.message,
+      }));
+
+      throw new GraphQLError('BAD_USER_INPUT', {
+        extensions: {
+          code: '400',
+          additionalInfo: zoderrors,
+        },
+      });
+    }
+
+    params.password = await argon2.hash(params.password);
+    const { name, email, password, birthDate } = params;
+    try {
       const newUser = await prisma.user.create({
         data: {
           name,
@@ -24,25 +37,28 @@ export class UserService {
 
       return newUser;
     } catch (error) {
-      console.log(error);
-      throw new GraphQLError('Erro Ao Criar Usuário', {
-        extensions: { code: 'Internal_Server_Error' },
-      });
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          const errorField = `${error.meta?.target} is already taken`;
+          throw new GraphQLError('BAD_USER_INPUT', {
+            extensions: {
+              code: '400',
+              message: errorField,
+            },
+          });
+        }
+      }
+      throw error;
     }
   }
 
   async showUsersService(): Promise<User[]> {
-    try {
-      const users = await prisma.user.findMany();
+    const users = await prisma.user.findMany();
 
-      if (users.length === 0) {
-        throw new Error('Nenhum usuário encontrado');
-      }
-
-      return users;
-    } catch (error) {
-      console.log(error);
-      throw error;
+    if (users.length === 0) {
+      throw new Error('Nenhum usuário encontrado');
     }
+
+    return users;
   }
 }
