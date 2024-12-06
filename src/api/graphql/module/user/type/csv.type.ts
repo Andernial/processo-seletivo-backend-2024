0e8f0ff69af4b44e6e110a96f45d21af3cad3b7f
Anderson <loonander@gmail.com>
2024-12-06 14:34:23 -0300

    fix: removed unused type

--- src/api/graphql/module/user/type/csv.type.ts
+++
@@ -1,8 +0,0 @@
-// import { Field, ObjectType } from 'type-graphql';
-// import { CsvModel } from '@domain/model';
-
-// @ObjectType({ description: 'Csv mutation response object' })
-// export class Csv implements CsvModel {
-//   @Field(() => String)
-//   csvFile: string;
-// }
